980ac936d25573f6bbb8b40ae12f25ab51324505
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-06-13 18:36:08 +0530

    first commit

---
+++ Hash Map Data Structures/myHash.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+struct MyHash {
+    int bucket;
+    list<int>* table; 
+
+    MyHash(int b) {
+        bucket = b;
+        table = new list<int>[b];
+    }
+
+    void insert(int key){
+        int i = key%bucket;
+        table[i].push_back(key);
+    }
+
+    bool search(int key){
+        int i = key%bucket;
+        for(int a: table[i]){
+            if(a==key)
+            return true;
+        }
+        return false;
+    }
+
+    void remove(int key){
+        int i=key%bucket;
+        table[i].remove(key);
+    }
+};
+
+int main() {
+}
