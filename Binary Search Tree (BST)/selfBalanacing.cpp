2254f436c93685d1873c13fdf297c9c92f8cafbd
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-06 08:10:08 +0530

    code

---
+++ Binary Search Tree (BST)/selfBalanacing.cpp
@@ -0,0 +1,9 @@
+//In this we do either right rotation or left rotation
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"There are two type of tree which maintain balance: "<<endl;
+    cout<<"AVL Tree"<<endl;
+    cout<<"Red Black Tree"<<endl;
+}
