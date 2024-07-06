5451d1cb4c8e44808965f1565190aaa1c5f8409c
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-06 22:24:34 +0530

    code

--- Heap Data Structures/insert.cpp
+++ Heap Data Structures/insert.cpp
@@ -23,19 +23,27 @@ class MinHeap{
     int parent(int i){
         return (i-1)/2;
     }
+    
     void insert(int x){
-        if(size==capacity){
-            cout<<"overflow"<<endl;
+        if(size == capacity){
+            cout << "Overflow" << endl;
             return;
         }
         size++;
-        arr[size-1]=x;
+        arr[size-1] = x;
         int i = size-1;
-        while(i!=0 and arr[parent(i)]>arr[i]){
-            swap(arr[i],arr[parent(i)]);
+        while(i != 0 && arr[parent(i)] > arr[i]){
+            swap(arr[i], arr[parent(i)]);
             i = parent(i);
         }
     }
+    
+    void printHeap() {
+        for (int i = 0; i < size; i++)
+            cout << arr[i] << " ";
+        cout << endl;
+    }
+    
     ~MinHeap() {
         delete[] arr;
     }
@@ -45,4 +53,6 @@ int main(){
     MinHeap h(2);
     h.insert(10);
     h.insert(20);
+    h.printHeap(); // Print the heap to verify
+    return 0;
 }
