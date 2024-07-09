354587ff159a6aa3ce4e7cce2bca5a0c0244a161
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-09 08:32:08 +0530

    code

---
+++ STL Stack/basic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    cout<<s.size()<<endl;
+    s.pop();
+    cout<<s.top()<<endl;
+    return 0;
+}
