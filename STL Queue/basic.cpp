6c45cc67444d006e35b5044242c4bedfd2f50e63
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-11 12:27:38 +0530

    code

---
+++ STL Queue/basic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(){
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    cout<<q.size()<<endl;
+    while(q.empty()==false){
+        cout<<q.front()<<" "<<q.back()<<endl;
+        q.pop();
+    }
+    return 0;
+}
