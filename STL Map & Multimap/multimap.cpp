db6f1d5f7634f11ec9f5eab82b573940d5756b31
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-12 22:42:22 +0530

    code

---
+++ STL Map & Multimap/multimap.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>  // Include map header
+
+int main() {
+    std::multimap<int, int> m; // Declare a multimap
+    // m.insert({10,20});
+    // m.insert({20,24});
+    // m.insert({10,23});
+    // same key value will be inserted two times in multimap
+}
