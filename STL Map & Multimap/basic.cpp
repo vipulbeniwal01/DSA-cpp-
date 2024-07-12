d5e728da5f25f9c785a630c8c5c7d33085753b65
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-12 10:45:47 +0530

    code

--- STL Map & Multimap/basic.cpp
+++ STL Map & Multimap/basic.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     map<int,int> m;
-    m.insert({10, 23}); //this works ki cpp-std 11 or later
+    // m.insert({10, 23}); //this works ki cpp-std 11 or later
     m[20]=40;
     for(auto &x:m){
         cout<<x.first<<" "<<x.second<<endl;
