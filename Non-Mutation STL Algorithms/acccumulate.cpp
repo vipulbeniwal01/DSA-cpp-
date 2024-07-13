738d497c559991040b14abfafbc8e7be554addea
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-13 05:49:40 +0530

    code

--- Non-Mutation STL Algorithms/acccumulate.cpp
+++ Non-Mutation STL Algorithms/acccumulate.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<algorithm>
+#include<numeric>
 using namespace std;
 
 int myComp(int x, int y){
@@ -9,10 +9,10 @@ int myComp(int x, int y){
 int main(){
     int arr[]={10,20,30};
     int sum=0;
-    // cout<<accumulate(arr,arr+3,sum); //this will add all the elements to sum
+    cout<<accumulate(arr,arr+3,sum)<<endl; //this will add all the elements to sum
     int sub=100;
-    //cout<<accumulate(arr,arr+3,sub,minus<int>); //this will minus all the elemnts from sub
+    // cout<<accumulate(arr,arr+3,sub,minus<int>); //this will minus all the elemnts from sub
     int mul=1;
-    //cout<<accumulate(arr,arr+3,mul,myComp); //this will multiply all the elements to mul
+    cout<<accumulate(arr,arr+3,mul,myComp); //this will multiply all the elements to mul
     return 0;
 }
