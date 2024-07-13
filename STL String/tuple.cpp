286bee48eef7fdf705d9e2fcb9e76e91e77c35fd
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-13 15:49:51 +0530

    code

---
+++ STL String/tuple.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<tuple>
+using namespace std;
+int main(){
+    // tuple<string,int,string> t=make_tuple("abc",10,"xyz");
+    // cout<<get<0>(t)<<" "<<get<1>(t)<<" "<<get<2>(t)<<endl;
+    // get<0>(t)="pqr";
+    // cout<<get<0>(t)<<endl;
+}
