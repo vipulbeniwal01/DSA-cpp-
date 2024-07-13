0632957ecfef5ab099ed868c1b0816d361082334
Vipul Beniwal <vipulbeniwal2004@gmail.com>
2024-07-13 12:48:54 +0530

    code

---
+++ Mutating STL Algorithms/next_permutation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5};
+    next_permutation(arr,arr+5);
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
